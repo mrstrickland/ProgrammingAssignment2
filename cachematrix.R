f6421cd23cddf11fb123379e7244d3ea920888d5
mrstrickland <steveandjo42@gmail.com>
2015-02-17 09:51:28 +0000

    Editted some annotations and played around with the formating

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,14 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv<-NULL	##Creates the inv value and sets it to NULL
-	set<-function(y){ ##Creates a function that sets the value of x and assigns inv as NULL again. Will be used to reset values when x is changed after the inverse of the previous x has been calculated.
+	set<-function(y){ ##Creates a function that sets the value of x outside of the set function environment and assigns inv as NULL again. 
+					  ##Will be used to reset values when x is changed after the inverse of the previous x has been calculated.
 	x<<-y
-	inv<-NULL
+	inv<<-NULL
 	}
-	get <- function() x ##Creates an empty function that reurns the value of x.      
-			setinv <- function(a) inv <<- a ##Creates a function that sets inv to be a value outside of the function environment.
-            getinv <- function() inv ##Creates and empty function that returns the value of inv.
+	get <- function() x ##Creates an empty function that reurns the value of x.     
+            setinv <- function(a) inv <<- a ##Creates a function that sets inv to be a value outside of the setinv function environment.
+			getinv <- function() inv ##Creates and empty function that returns the value of inv.
             list(set = set, get = get, ##Outputs a list of the four functions created in makeCacheMatrix
                  setinv = setinv,
                  getinv = getinv)
@@ -22,11 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 	inv <- x$getinv() ##Sets inv as the value from the getinv function of the input
-    if(!is.null(inv)) { ##Tests whether inv is not the value NULL - if this is the case if retrieves the value from the cache as it has already been calculated.
-			message("retrieving cache data")
+    if(!is.null(inv)) { ##Tests whether inv is not the value NULL - if this is the case if retrieves the value from 
+	                    ##the cache as it has already been calculated.
+            message("retrieving cache data")
             return(inv) ##Returns the value of the inverse from the Cache
             }
-    data <- x$get() ##If the inverse is not already in the cache, it uses the x$get() function to get the value of the matrix x, originally entered into makeCacheMatrix, assigning it to variable data.
+    data <- x$get() ##If the inverse is not already in the cache, use the x$get() function to get the value of the matrix x, 
+	                ##originally entered into makeCacheMatrix, assigning it to variable data.
     inv <- solve(data, ...) ##inv is assigned as the inverse of the the original matrix (now stored in variable data).
     x$setinv(inv) ## The inverse value is stored in the cache, using the setinv function defined previously.
     inv ##inv is returned
