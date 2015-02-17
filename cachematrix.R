57746847544cba07468ce5c5b55a8baf2e58a468
mrstrickland <steveandjo42@gmail.com>
2015-02-17 10:09:25 +0000

    Played with the formatting some more

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,16 @@
 makeCacheMatrix <- function(x = matrix()) {
 	inv<-NULL	##Creates the inv value and sets it to NULL
 	set<-function(y){ ##Creates a function that sets the value of x outside of the set function environment and assigns inv as NULL again. 
-					  ##Will be used to reset values when x is changed after the inverse of the previous x has been calculated.
+                      ##Will be used to reset values when x is changed after the inverse of the previous x has been calculated.
 	x<<-y
 	inv<<-NULL
 	}
 	get <- function() x ##Creates an empty function that reurns the value of x.     
-            setinv <- function(a) inv <<- a ##Creates a function that sets inv to be a value outside of the setinv function environment.
-			getinv <- function() inv ##Creates and empty function that returns the value of inv.
-            list(set = set, get = get, ##Outputs a list of the four functions created in makeCacheMatrix
-                 setinv = setinv,
-                 getinv = getinv)
+    setinv <- function(a) inv <<- a ##Creates a function that sets inv to be a value outside of the setinv function environment.
+    getinv <- function() inv ##Creates and empty function that returns the value of inv.
+    list(set = set, get = get, ##Outputs a list of the four functions created in makeCacheMatrix
+         setinv = setinv,
+         getinv = getinv)
     }
 
 ## Once running a particular matrix through the makeCacheMatrix function, the function cacheSolve can then be applied to the resultant list of functions.
@@ -29,7 +29,7 @@ cacheSolve <- function(x, ...) {
             return(inv) ##Returns the value of the inverse.
             }
     data <- x$get() ##If the inverse is not already in the cache, use the x$get() function to get the value of the matrix x, 
-	                ##originally entered into makeCacheMatrix, assigning it to variable data.
+                    ##originally entered into makeCacheMatrix, assigning it to variable data.
     inv <- solve(data, ...) ##inv is assigned as the inverse of the the original matrix (now stored in variable data).
     x$setinv(inv) ## The inverse value is stored in the cache, using the setinv function defined previously.
     inv ##inv is returned
